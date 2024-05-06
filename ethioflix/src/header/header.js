f186b9381f69c8040b887535e02e528e782fc9ea
meta-bay <bayiemeseret@gmail.com>
2024-05-06 19:53:25 +0300

    changing the header

--- ethioflix/src/header/header.js
+++ ethioflix/src/header/header.js
@@ -4,30 +4,42 @@ import './header.css';
 
 function Header() {
   return (
-    <header>
-       <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                    <li> 
-                    <Link to="/genre">Genre</Link> 
-
-                    </li>
-                    <li>
-                        <Link to="/country">Country</Link>   
-                    </li>
-                    <li>
-                        <Link to="/category">Category</Link>
-                    </li>
-                <li><Link to="/about">About</Link></li>
-            </ul>
-        </nav>
-        <Link to="/login">
-          <button style={{ display: 'inline-block' }}>Log In</button>
-        </Link>
-        <Link to="/signup">
-          <button style={{ display: 'inline-block' }}>Sign Up</button>
-        </Link>
-    </header>
+    <nav className="navbar">
+        <h1>Ethioflix</h1>
+        <div className="navtab">
+            <a href="/">Home</a>
+            <div className="dropdown">
+                <button className="dropbtn">Category</button>
+                <div className="dropdown-content">
+                    <div className="home">
+                        <h4>Home</h4>
+                        <a href="#">Trending</a>
+                        <a href="#">Popular</a>
+                        <a href="#">Upcoming</a>
+                    </div>
+                    <div className="genre">
+                        <h4>Genre</h4>
+                        <a href="#">Action</a>
+                        <a href="#">Adventure</a>
+                        <a href="#">Crime</a>
+                        <a href="#">Drama</a>
+                        <a href="#">Fantasy</a>
+                        <a href="#">Horror</a>
+                        <a href="#">Thriller</a>
+                        <a href="#">Sci-Fi</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="searchbar">
+            <input type="text" placeholder="Search " />
+            <button><a href="">Search</a></button>
+        </div>
+        <div className="auth">
+            <button><a href="">Log in</a></button>
+            <button><a href="">Sign up</a></button>
+        </div>
+    </nav>
   )
 }
 
